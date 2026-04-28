c6c753d3f3230de7c1aa26d6b64d5670669eb280
Edri Kopliku <edrikop@gmail.com>
2026-04-28 15:49:54 +0200

    Added a GPT model class, renamed the files, halfway through implementing the training pipeline

--- step3_attention_layer.py
+++ step3_attention_layer.py
@@ -13,19 +13,19 @@ class CausalSelfAttention(nn.Module):
         self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.dropout = nn.Dropout(dropout) # New
-        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) 
 
     def forward(self, x):
-        b, n_tokens, d_in = x.shape # New batch dimension b
+        b, n_tokens, d_in = x.shape 
         keys = self.W_key(x)
         queries = self.W_query(x)
         values = self.W_value(x)
 
-        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
-        attn_scores.masked_fill_(  # New, _ ops are in-place
+        attn_scores = queries @ keys.transpose(1, 2) 
+        attn_scores.masked_fill_(  
             self.mask.bool()[:n_tokens, :n_tokens], -torch.inf) 
         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
-        attn_weights = self.dropout(attn_weights) # New
+        attn_weights = self.dropout(attn_weights) 
 
         context_vec = attn_weights @ values
         return context_vec
@@ -51,63 +51,55 @@ class MultiHeadAttention(nn.Module):
 
         self.d_out = d_out
         self.num_heads = num_heads
-        self.head_dim = d_out // num_heads # Reduce the projection dim to match desired output dim
+        self.head_dim = d_out // num_heads 
 
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
-        self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
+        self.out_proj = nn.Linear(d_out, d_out) 
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, x):
         b, num_tokens, d_in = x.shape
 
-        keys = self.W_key(x) # Shape: (b, num_tokens, d_out)
+        keys = self.W_key(x)
         queries = self.W_query(x)
         values = self.W_value(x)
 
-        # We implicitly split the matrix by adding a `num_heads` dimension
-        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
         keys = keys.view(b, num_tokens, self.num_heads, self.head_dim) 
         values = values.view(b, num_tokens, self.num_heads, self.head_dim)
         queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
 
-        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
         keys = keys.transpose(1, 2)
         queries = queries.transpose(1, 2)
         values = values.transpose(1, 2)
 
-        # Compute scaled dot-product attention (aka self-attention) with a causal mask
         attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
         
-        # Original mask truncated to the number of tokens and converted to boolean
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
 
-        # Use the mask to fill attention scores
         attn_scores.masked_fill_(mask_bool, -torch.inf)
         
         attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
 
-        # Shape: (b, num_tokens, num_heads, head_dim)
         context_vec = (attn_weights @ values).transpose(1, 2) 
         
-        # Combine heads, where self.d_out = self.num_heads * self.head_dim
         context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
         context_vec = self.out_proj(context_vec) # optional projection
 
         return context_vec
 
-torch.manual_seed(123)
+# torch.manual_seed(123)
 
-context_length = max_length
-d_in = output_dim
-d_out = d_in
+# context_length = max_length
+# d_in = output_dim
+# d_out = d_in
 
-mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
+# mha = MultiHeadAttention(d_in, d_out, context_length, 0.0, num_heads=2)
 
-batch = input_embeddings
-context_vecs = mha(batch)
+# batch = input_embeddings
+# context_vecs = mha(batch)
 
-print("context_vecs.shape:", context_vecs.shape)
+# print("context_vecs.shape:", context_vecs.shape)
